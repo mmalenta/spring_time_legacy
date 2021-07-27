9e12d4772e14af2468514f9b0649df8263cbd112
Mateusz Malenta <mateusz.malenta@gmail.com>
2021-07-27 18:25:25 +0100

    New logging formatter
    Brought the logging formatter in line with the rest of post-processing. Uses colour-coded logging levels and additional information on processes and modules.

--- bin/spring_supervisor.py
+++ bin/spring_supervisor.py
@@ -8,6 +8,22 @@ from supervisor.supervisor import Supervisor
 
 logger = logging.getLogger()
 
+class ColouredFormatter(logging.Formatter):
+
+  custom_format = "[%(asctime)s] [%(process)d %(processName)s] [\033[1;{0}m%(levelname)s\033[0m] [%(module)s] %(message)s"
+
+  def format(self, record):
+
+    colours = {
+      logging.DEBUG: 30,
+      logging.INFO: 32,
+      logging.WARNING: 33,
+      logging.ERROR: 31
+    }
+
+    colour_number = colours.get(record.levelno)
+    return logging.Formatter(self.custom_format.format(colour_number), datefmt="%a %Y-%m-%d %H:%M:%S").format(record)
+
 def main():
 
   parser = ap.ArgumentParser(description="MeerTRAP supervisor for the Spring post-processing pipeline",
@@ -46,10 +62,8 @@ def main():
   arguments = parser.parse_args()
   logger.setLevel(getattr(logging, arguments.log.upper()))
   handler = logging.StreamHandler()
-  formatter = logging.Formatter("%(asctime)s, %(levelname)s: %(message)s",
-                                datefmt="%a %Y-%m-%d %H:%M:%S")
   handler.setLevel(getattr(logging, arguments.log.upper()))
-  handler.setFormatter(formatter)
+  handler.setFormatter(ColouredFormatter())
   logger.addHandler(handler)
   logger.debug("Logging set up")
 
